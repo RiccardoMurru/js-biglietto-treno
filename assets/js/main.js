14abfc27ac1a46ff8d65ddfd7af426b31cb5b859
Riccardo Murru <riccardo.mr@gmail.com>
2020-03-26 16:17:42 +0100

    add. prints (getElement)

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,7 @@
  var age;
  var grossPrice;
  var netPrice;
+ var percent;
 
  // 1. Chiedere all'utente i km da percorrere e l'età
 
@@ -30,15 +31,35 @@
  // se > 18, sconto = 20%
  if (age < 18) {
     netPrice = grossPrice - (grossPrice * .2);
+    percent = '20%';
     console.log ('biglietto scontato del 20%: € ' + netPrice);
+    console.log('percentuale di sconto: ' + percent);
+    
  } else if (age > 65) {
     // se > 65, sconto = 40%;
     netPrice = grossPrice - (grossPrice * .4);
+    percent = '40%';
     console.log ('biglietto scontato del 40%: € ' + netPrice);
+    console.log('percentuale di sconto: ' + percent);
+    
  } else {
     // nessuno sconto applicato
     netPrice = grossPrice;
+    percent = 0;
     console.log ('biglietto non scontato: € ' + netPrice);
+    console.log('percentuale di sconto: ' + percent);
+    
  }
 
+
+ // 4. Stampare prezzo biglietto e indicare l'eventuale sconto
+
+ document.getElementById('price').innerHTML = netPrice.toFixed(2);
  
+ if (percent == '20%') {
+    document.getElementById('discount').innerHTML = 'Hai ottenuto uno sconto del ' + percent + ' perché sei minorenne.';
+ } else if (percent == '40%') {
+     document.getElementById('discount').innerHTML = 'Hai ottenuto uno sconto del ' + percent + ' perché sei over 65.';
+ } else {
+     document.getElementById('discount').innerHTML = 'Non hai ottenuto nessuno sconto.';
+ }
